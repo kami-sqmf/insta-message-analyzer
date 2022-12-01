7867bfb0fc064cdc1c1bb90a09a2df768221b377
Kami-Sqmf <kamisqmf@gmail.com>
2022-12-01 12:12:16 +0100

    Stable 1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useRef, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 const utf8 = require('utf8');
 
@@ -8,6 +8,7 @@ const strangerMessage = new Map<string, messageBase[][]>()
 const userInfo = {
   name: "無法取得"
 }
+const avatars = [...getRandomAvatarURLArray(), ...getRandomAvatarURLArray(), ...getRandomAvatarURLArray(), ...getRandomAvatarURLArray(), ...getRandomAvatarURLArray(), ...getRandomAvatarURLArray(), ...getRandomAvatarURLArray()]
 
 function App() {
   const [showing, setShowing] = useState("Main")
@@ -82,40 +83,31 @@ const Main = ({ setShowing }: { setShowing: Dispatch<SetStateAction<string>> }) 
     }
   }
   return (
-    <div>
-      <input directory="" webkitdirectory="" multiple type="file" id="filepicker" name="fileList" ref={fileInput} onChange={(e: any) => fileReader(e)} />
+    <div className='w-full h-64 px-8 py-8 flex flex-col justify-around'>
+      <div>
+        <span className='mr-2'>1. 先登入至 Instagram 帳號 (已登入可跳過)</span>
+        <span className='text-primary font-bold hover:text-secondary'><a href='https://www.instagram.com/' target="_blank" rel="noopener noreferrer">https://www.instagram.com/</a></span>
+      </div>
+      <div>
+        <span className='mr-2'>2. 請先下載 JSON 備份資料檔</span>
+        <span className='text-primary font-bold hover:text-secondary'><a href='https://www.instagram.com/download/request/' target="_blank" rel="noopener noreferrer">https://www.instagram.com/download/request/</a></span>
+      </div>
+      <p>3. 然後解壓縮下載的檔案</p>
+      <div className='px-4 py-3 bg-light text-primary font-black hover:scale-105 transition-all w-max cursor-pointer rounded hover:text-secondary' onClick={() => fileInput.current?.click()}>4. 按此選擇以解壓縮的資料夾</div>
+      <input hidden directory="" webkitdirectory="" multiple type="file" id="filepicker" name="fileList" ref={fileInput} onChange={(e: any) => fileReader(e)} />
+      <p className='text-gray-400 text-xs'>* 本操作不會上傳任何資料，只在本地執行。詳情請看原始碼。</p>
     </div>
   )
 }
 
 const DataSelection = () => {
-  const [analysis, setAnalysis] = useState({
-    "target": "02",
-    "message": {
-      "length": 58725,
-      "totalWords": 358498,
-      "avgWords": 6.1046913580246915,
-      "sentLength": 26346,
-      "recievedLength": 32379,
-      "sentTotalWords": 155267,
-      "recievedTotalWords": 203231,
-      "sentAvgWords": 5.893380399301602,
-      "recievedAvgWords": 6.276629914450724
-    },
-    "call": {
-      "length": 289,
-      "totalSeconds": 5731381,
-      "avgSeconds": 19831.768166089965
-    },
-    "startDay": 1636512605913,
-    "endDay": 1669218284049,
-    "totalDay": 378.5379413888889
-  } as any)
+  const [analysis, setAnalysis] = useState(null as any)
   const [error, setError] = useState("")
-  const renderAnalysis = (messageArrays: messageBase[][], userName: string) => {
+  const renderAnalysis = (messageArrays: messageBase[][], userName: string, avatars: string) => {
     try {
       const analyzed = {
-        targeet: userName,
+        target: userName,
+        avatars: avatars,
         message: {
           length: 0,
           totalWords: 0,
@@ -135,21 +127,35 @@ const DataSelection = () => {
         startDay: 0,
         endDay: 0,
         totalDay: 0,
+        reactions: {
+          sent: [""],
+          recevied: [""]
+        }
       }
       setError("")
-      const messages: { type: "Message" | "Media" | "Call" | "Other"; content: string | null; timestamp: number; callDuration: number | null, recevied: Boolean }[] = new Array();
+      const messages: { type: "Message" | "Media" | "Call" | "Other"; content: string | null; timestamp: number; callDuration: number | null; recevied: Boolean; sentReactions: string | null; recievedReactions: string | null; }[] = new Array();
       for (const messageArray of messageArrays) {
         for (const message of messageArray) {
           let type: "Message" | "Media" | "Call" | "Other" = "Other";
+          let sentReactions: string | null = null;
+          let recievedReactions: string | null = null;
           if (message.bumped_message_metadata.bumped_message) type = "Message";
           else if (message.photos || message.videos || message.audio_files || message.share) type = "Media";
           else if (message.call_duration) type = "Call"
+          if (message.reactions) {
+            for (const reaction of message.reactions) {
+              if (utf8.decode(reaction.actor) == userInfo.name) sentReactions = reaction.reaction
+              else recievedReactions = reaction.reaction
+            }
+          }
           messages.push({
             type: type,
             content: message.content ? utf8.decode(message.content) : null,
             timestamp: message.timestamp_ms,
             callDuration: type.startsWith("Call") ? message.call_duration! : null,
             recevied: utf8.decode(message.sender_name) == userInfo.name ? false : true,
+            sentReactions: sentReactions,
+            recievedReactions: recievedReactions
           })
         }
       }
@@ -171,15 +177,26 @@ const DataSelection = () => {
           }
         } else if (message.callDuration) {
           analyzed.call.length++;
-          analyzed.call.totalSeconds += message.callDuration;
+          analyzed.call.totalSeconds += message.callDuration / 1000;
         }
       }
+      analyzed.reactions.recevied = (Object.fromEntries(
+        Object.entries(messages.reduce(function (prev: any, cur) {
+          if (cur.recievedReactions) prev[cur.recievedReactions] = (prev[cur.recievedReactions] || 0) + 1;
+          return prev;
+        }, {})).sort(([, a], [, b]) => b as any - (a as any))) as any
+      );
+      analyzed.reactions.sent = (Object.fromEntries(
+        Object.entries(messages.reduce(function (prev: any, cur) {
+          if (cur.sentReactions) prev[cur.sentReactions] = (prev[cur.sentReactions] || 0) + 1;
+          return prev;
+        }, {})).sort(([, a], [, b]) => b as any - (a as any))) as any
+      );
       analyzed.message.avgWords = analyzed.message.totalWords / analyzed.message.length;
       analyzed.call.avgSeconds = analyzed.call.totalSeconds / analyzed.call.length;
       analyzed.message.recievedAvgWords = analyzed.message.recievedTotalWords / analyzed.message.recievedLength;
       analyzed.message.sentAvgWords = analyzed.message.sentTotalWords / analyzed.message.sentLength;
       analyzed.totalDay = (analyzed.endDay - analyzed.startDay) / 1000 / 60 / 60 / 24;
-      console.log(analyzed)
       setAnalysis(analyzed);
     } catch (e) {
       setError(e as any)
@@ -188,13 +205,14 @@ const DataSelection = () => {
   return (
     <div className='grid grid-cols-3 mt-6 rounded border border-secondary/60'>
       <div className='col-span-1 border-r-2 border-greendark overflow-y-scroll overflow-x-clip max-h-screen'>
-        <div className='flex flex-col items-center justify-center'>
-          <p className='py-3 px-4 w-full text-center border-b-2 text-primary font-medium'>{userInfo.name}</p>
-          {Array.from(usersMessage.keys()).sort((a, b) => usersMessage.get(b)![usersMessage.get(b)!.length - 1][0].timestamp_ms - usersMessage.get(a)![usersMessage.get(a)!.length - 1][0].timestamp_ms).map((username) => {
+        <div className='flex flex-col items-center justify-center relative'>
+          <p className='py-3 px-4 w-full text-center border-b-2 text-primary font-bold sticky'>{userInfo.name}</p>
+          {Array.from(usersMessage.keys()).sort((a, b) => usersMessage.get(b)![usersMessage.get(b)!.length - 1][0].timestamp_ms - usersMessage.get(a)![usersMessage.get(a)!.length - 1][0].timestamp_ms).map((username, key) => {
             const data = usersMessage.get(username);
+            const avatar = avatars[key];
             return (
-              <div className='md:px-3 lg:px-6 py-4 flex flex-row w-full space-x-2 cursor-pointer hover:bg-gray-200' onClick={(e) => renderAnalysis(data!, username)}>
-                <img className='h-12 w-12 rounded-full' src={getRandomAvatarURL()} alt="隨機大頭貼" />
+              <div className='md:px-3 lg:px-6 py-4 flex flex-row w-full space-x-2 cursor-pointer hover:bg-gray-200' onClick={(e) => renderAnalysis(data!, username, avatar)} key={key}>
+                <img className='h-12 w-12 rounded-full' src={avatar} alt="隨機大頭貼" />
                 <div className='flex flex-col justify-start items-start w-full'>
                   <span className='text-primary font-bold'>{username}</span>
                   <div className='w-full grid grid-cols-3'>
@@ -209,7 +227,11 @@ const DataSelection = () => {
       </div>
       <div className='col-span-2'>
         {error && <Error error={error} />}
-        <div className='flex-col px-8 py-5 space-y-4'>
+        {analysis && <div className='flex-col px-8 py-3 space-y-4'>
+          <div className='w-full text-center border-b-2 text-primary font-medium sticky flex flex-row items-center pb-2'>
+            <img alt='Random' src={analysis.avatars} className="w-6 h-6 rounded-full"></img>
+            <span className='font-bold text-primary text-lg ml-3'>{analysis.target}</span>
+          </div>
           <div className='border-2 border-secondary bg-gradient-to-r from-light/50 via-light to-light/70 rounded-md p-2 flex flex-row space-x-8 w-max'>
             <div className='flex flex-col'>
               <p className='font-bold text-primary'>訊息數</p>
@@ -225,7 +247,7 @@ const DataSelection = () => {
             </div>
             <div className='flex flex-col'>
               <p className='font-bold text-primary'>通話時長</p>
-              <span className='font-black text-xl text-primary/60'>{analysis.call.totalSeconds}秒</span>
+              <span className='font-black text-xl text-primary/60'>{timeFormatter(analysis.call.totalSeconds)}</span>
             </div>
           </div>
           <div className='border-2 border-secondary bg-gradient-to-r from-light/90 via-light/50 to-light/70 rounded-md p-2 flex flex-row space-x-8 w-max'>
@@ -256,7 +278,27 @@ const DataSelection = () => {
               <span className='font-black text-xl text-primary/60'>{analysis.message.recievedAvgWords.toFixed(2)}</span>
             </div>
           </div>
-        </div>
+          <div className='border-2 border-secondary bg-gradient-to-r from-light/70 via-light/50 to-light/90 rounded-md p-2 flex flex-row space-x-8 w-max items-center'>
+            <p className='font-bold text-primary'>你最常按的5個表符</p>
+            {Object.keys(analysis.reactions.sent).slice(0, 5).map((reaction, key) => (
+              <div key={key}>
+                <p className='text-lg'>{utf8.decode(reaction)}</p>
+                <p className='font-black text-xl text-primary/60'>{analysis.reactions.sent[reaction]} 次</p>
+              </div>
+            ))}
+            {Object.keys(analysis.reactions.sent).length == 0 && <p className='font-black text-xl text-primary/60'>沒有按過啊！！</p>}
+          </div>
+          <div className='border-2 border-secondary bg-gradient-to-r from-light/70 via-light/50 to-light/90 rounded-md p-2 flex flex-row space-x-8 w-max items-center'>
+            <p className='font-bold text-primary'>{analysis.target} 最常按的5個表符</p>
+            {Object.keys(analysis.reactions.recevied).slice(0, 5).map((reaction, key) => (
+              <div key={key}>
+                <p className='text-lg'>{utf8.decode(reaction)}</p>
+                <p className='font-black text-xl text-primary/60'>{analysis.reactions.recevied[reaction]} 次</p>
+              </div>
+            ))}
+            {Object.keys(analysis.reactions.recevied).length == 0 && <p className='font-black text-xl text-primary/60'>沒有按過啊！！</p>}
+          </div>
+        </div>}
       </div>
     </div>
   )
@@ -287,8 +329,23 @@ function readFileAsync(file: File) {
   })
 }
 
-function getRandomAvatarURL() {
-  return `/avatar/${Math.floor(Math.random() * 64)}.png`
+function getRandomAvatarURLArray() {
+  const arr = []
+  for (let i = 0; i < 65; i++) {
+    arr.push(`/avatar/${i}.png`)
+  }
+  return (shuffle(arr))
+}
+
+function shuffle(a: Array<string>) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 function getRecentMessage(message: messageBase) {
@@ -298,4 +355,17 @@ function getRecentMessage(message: messageBase) {
   else if (message.call_duration) return "跟你來個通話";
   else if (message.share) return "跟你分享了一些好康的";
   else if (message.videos) return "傳送了一個影片";
+}
+
+function timeFormatter(seconds: number) {
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor(seconds % (3600 * 24) / 3600);
+  var m = Math.floor(seconds % 3600 / 60);
+  var s = Math.floor(seconds % 60);
+
+  var dDisplay = d > 0 ? d + "天 " : "";
+  var hDisplay = h > 0 ? h + "時 " : "";
+  var mDisplay = m > 0 ? m + "分 " : "";
+  var sDisplay = s + "秒 ";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
 }
